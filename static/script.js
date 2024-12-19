45295a619077ab7fb30839bad955cfd9ffa15b6e
Dhruv Gandhi <63443072+gandhidhruv2003@users.noreply.github.com>
2024-12-19 20:22:00 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,12 +1,3 @@
-document.getElementById("signupBtn").addEventListener("click", function() {
-  window.location.href = "/signup";
-});
-
-document.getElementById("loginBtn").addEventListener("click", function() {
-  window.location.href = "/login";
-});
-
-
 document.getElementById("loginForm").addEventListener("submit", function(event) {
   event.preventDefault();
   window.location.href = "/homepage"; 
